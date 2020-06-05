23212b98ee1b8c13f09e35f08308313e2ffe11be
ctacedo <61666155+ctacedo@users.noreply.github.com>
2020-06-04 17:03:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Chaos00$",
+    password: "",
     database: "employee_tracker"
 });
 
